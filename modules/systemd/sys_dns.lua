ff5d4ce8eb77b260d13c9f57c70cfe9d8eb4683a
Eduardo Tongson <propolice@gmail.com>
2021-04-30 14:18:13 +0800

    Renamed to sys_dns.

--- modules/systemd/sys_dns.lua
+++ modules/systemd/sys_dns.lua
@@ -23,13 +23,13 @@ local crypto = require("crypto")
 Corefile = crypto.base64_encode(Corefile)
 return {
 	volumes = {
-		["__dns-config"] = {
+		["sys_dns-config"] = {
 			([[printf "%s" | base64 --decode > __MOUNTPOINT__/Corefile]]):format(Corefile),
 		}
 	},
 unit = [==[
 [Unit]
-Description=__dns Container
+Description=sys_dns Container
 Wants=network.target
 After=network-online.target
 
@@ -40,7 +40,7 @@ RestartSec=5
 TimeoutStartSec=infinity
 TimeoutStopSec=120
 Type=forking
-PIDFile=/run/podman-__dns.pid
+PIDFile=/run/podman-sys_dns.pid
 SystemCallArchitectures=native
 MemoryDenyWriteExecute=yes
 LockPersonality=yes
@@ -59,20 +59,20 @@ RestrictSUIDSGID=yes
 ProtectKernelTunables=yes
 #PrivateDevices=yes
 RestrictAddressFamilies=AF_INET
-ExecStartPre=-/usr/bin/podman stop -i __dns
-ExecStartPre=-/usr/bin/podman rm -i -v -f __dns
-ExecStop=/usr/bin/podman stop -t 12 __dns
-ExecStopPost=-/usr/bin/podman rm -i -v -f __dns
-ExecStart=/usr/bin/podman run --name __dns \
---security-opt seccomp=/etc/podman.seccomp/__dns.json \
+ExecStartPre=-/usr/bin/podman stop -i sys_dns
+ExecStartPre=-/usr/bin/podman rm -i -v -f sys_dns
+ExecStop=/usr/bin/podman stop -t 12 sys_dns
+ExecStopPost=-/usr/bin/podman rm -i -v -f sys_dns
+ExecStart=/usr/bin/podman run --name sys_dns \
+--security-opt seccomp=/etc/podman.seccomp/sys_dns.json \
 --replace \
 --network host \
---hostname __dns  \
+--hostname sys_dns  \
 --cap-drop all \
 --cap-add net_bind_service \
---conmon-pidfile=/run/podman-__dns.pid \
+--conmon-pidfile=/run/podman-sys_dns.pid \
 -e "TZ=UTC" \
---volume __dns-config:/config \
+--volume sys_dns-config:/config \
 --cpuset-cpus __CPUS__ \
 --memory __MEM__ \
 __ID__
