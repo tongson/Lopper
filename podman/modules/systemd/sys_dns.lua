cfc4b5df52b37fc068b174dc2b82d41d619eee73
Eduardo Tongson <propolice@gmail.com>
2021-05-02 16:24:11 +0800

    sys_dns should use its own unit.

--- podman/modules/systemd/sys_dns.lua
+++ podman/modules/systemd/sys_dns.lua
@@ -27,8 +27,54 @@ return {
 			([[printf "%s" | base64 --decode > __MOUNTPOINT__/Corefile]]):format(Corefile),
 		}
 	},
-	mounts = {
-		["sys_dns-config"] = "/config",
-	},
-	capabilities = { "net_bind_service" },
+	unit = [==[
+[Unit]
+Description=__NAME__ Container
+Wants=network.target
+After=network-online.target
+
+[Service]
+Environment=PODMAN_SYSTEMD_UNIT=%n
+Restart=on-failure
+RestartSec=5
+Type=notify
+NotifyAccess=all
+KillMode=mixed
+SystemCallArchitectures=native
+MemoryDenyWriteExecute=yes
+LockPersonality=yes
+NoNewPrivileges=yes
+RemoveIPC=yes
+DevicePolicy=closed
+PrivateTmp=yes
+PrivateNetwork=false
+ProtectKernelModules=yes
+ProtectSystem=full
+ProtectHome=yes
+ProtectKernelLogs=yes
+ProtectClock=yes
+RestrictRealtime=yes
+RestrictSUIDSGID=yes
+ProtectKernelTunables=yes
+RestrictAddressFamilies=AF_INET
+ExecStart=/usr/bin/podman run --name __NAME__ \
+--security-opt seccomp=/etc/podman.seccomp/__NAME__.json \
+--security-opt apparmor=unconfined \
+--security-opt label=disable \
+--rm \
+--replace \
+--sdnotify conmon \
+--network host \
+--hostname sys_dns \
+--cpu-shares __SHARES__ \
+--cpuset-cpus __CPUS__ \
+--memory __MEM__ \
+--cap-drop all \
+--cap-add net_bind_service \
+--volume sys_dns-config:/config \
+__ID__
+
+[Install]
+WantedBy=multi-user.target
+]==],
 }
